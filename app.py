6d6a6bc6e39cad65e94faf9b3400d8be445bc611
LeonardoSilva <apx88.0@gmail.com>
2020-06-18 16:51:47 -0400

    Added data

--- app.py
+++ app.py
@@ -11,20 +11,21 @@ class BaseModel(Model):
 
 class Beer(BaseModel):
     name = CharField()
-    # brewery_type = CharField()
+    brewery_type = CharField()
     street = CharField()
-    # city = CharField()
-    # state = CharField()
-    # postal_code = IntegerField()
-    # country = CharField()
-    # longitude = IntegerField()
-    # latitude = IntegerField()
-    # phone = IntegerField()
-    # website_url = CharField()
+    city = CharField()
+    state = CharField()
+    postal_code = IntegerField()
+    country = CharField()
+    longitude = IntegerField()
+    latitude = IntegerField()
+    phone = IntegerField()
+    website_url = CharField()
 
 
 db.connect()
 db.drop_tables([Beer])
 db.create_tables([Beer])
 
-Beer(name='test', street='testing').save()
+Beer(name='test', brewery_type="moretest", street='testing', city="test",
+     state="test", postal_code=20853, country="test", longitude=5, latitude=5, phone=3, website_url='test').save()
